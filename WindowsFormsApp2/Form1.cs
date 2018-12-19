bc0cad82d62229c19b1377eec92b481ac198e79c
likhithakosanam <kosanamlikhitha@gmail.com>
2018-12-19 13:07:17 +0530

    First Push

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        private object likhitha;
+        private object Desktop;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public object Users { get; private set; }
+
+        public void button1_Click_1(object sender, EventArgs e)
+        {
+            String name = textBox1.Text;
+            String regNo = textBox2.Text;
+            String state = comboBox2.Text;
+            String course = comboBox1.Text;
+           
+            String bsc = RB3.Text;
+            String bba = RB4.Text;
+            String bcom = RB5.Text;
+            DateTime DOB;
+            DOB = date1.Value;
+           
+            if (RB3.Checked)
+                RB3.Text = bsc;
+            else if (RB4.Checked)
+                RB3.Text = bba;
+            else if (RB5.Checked)
+                RB3.Text = bcom;
+            MessageBox.Show("Details saving successful");
+            /*String connetionString;
+            SqlConnection conn;
+            connetionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\likhitha\Documents\hostel1.mdf;Integrated Security=True;Connect Timeout=30";
+            conn = new SqlConnection(connetionString);
+            SqlCommand cmd = new SqlCommand("[dbo].[Procedure]", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            //C:\Users\likhitha\source\repos\WindowsFormsApp4\WindowsFormsApp4\Database1_log.ldf
+            cmd.Parameters.AddWithValue("name", textBox1.Text);
+            cmd.Parameters.AddWithValue("regno", textBox2.Text);
+            cmd.Parameters.AddWithValue("gender", RB1.Text );
+            cmd.Parameters.AddWithValue("state", comboBox2.Text);
+            cmd.Parameters.AddWithValue("DOB", date1.Value);
+            cmd.Parameters.AddWithValue("degree", RB3.Text);
+            cmd.Parameters.AddWithValue("course", comboBox1.Text);
+            cmd.Parameters.AddWithValue("room", comboBox3.Text);
+            conn.Open();
+            int s = cmd.ExecuteNonQuery();*/
+            String Querry = " INSERT INTO TESTING VALUES('" +textBox1.Text+"','"+textBox2.Text+"');";
+
+            SqlConnection sqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\likhitha\Desktop\c# programs\WindowsFormsApp2\WindowsFormsApp2\hostel1.mdf;Integrated Security=True");
+            sqlConnection.Open();
+            SqlCommand sqlCommand = new SqlCommand(Querry, sqlConnection);
+            sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
+
+            Form2 newFrm2 = new Form2();
+            newFrm2.Show();
+            this.Hide();
+        }
+
+        public void RESET_Click(object sender, EventArgs e)
+        {
+            String name = textBox1.Text;
+            String regNo = textBox2.Text;
+            String state = comboBox2.Text;
+            String course = comboBox1.Text;
+            String bsc = RB3.Text;
+            String bba = RB4.Text;
+            String bcom = RB5.Text;
+            DateTime DOB;
+            DOB = date1.Value;
+            //button1_Click_1(object sender, EventArgs e);
+            //RESET_Click obj = new RESET_Click();
+            //obj.button1_Click_1();
+            string message = "Do you want to reset everything?";
+            string title = "Reset Window";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+
+            DialogResult result = MessageBox.Show(message, title, buttons);
+            if (result == DialogResult.Yes)
+            {
+                textBox1.Clear();
+                textBox2.Clear();
+               
+                RB3.Checked = false;
+                RB4.Checked = false;
+                RB5.Checked = false;
+                comboBox1.Text="";
+                comboBox2.Text = "";
+               
+            }
+            else
+            {
+                textBox1.Text = name;
+                textBox2.Text = regNo;
+                comboBox1.Text= course;
+                comboBox2.Text = state;
+                if (RB3.Checked)
+                    RB3.Text = bsc;
+                else if(RB4.Checked)
+                    RB3.Text = bba;
+                else if (RB5.Checked)
+                    RB3.Text = bcom;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String name = textBox1.Text;
+            String regNo =textBox2.Text;
+            String state = comboBox2.Text;
+            String course = comboBox1.Text;
+            String bsc = RB3.Text;
+            String bba = RB4.Text;
+            String bcom = RB5.Text;
+            DateTime DOB;
+            DOB = date1.Value;
+            string message = "Do you want to close this window?";
+            string title = "Close Window";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+
+            DialogResult result = MessageBox.Show(message, title, buttons);
+            if (result == DialogResult.Yes)
+            {
+                this.Close();
+            }
+            else
+            {
+                textBox1.Text = name;
+                textBox2.Text = Convert.ToString(regNo);
+                comboBox1.Text = course;
+                comboBox2.Text = state;
+               
+                if (RB3.Checked)
+                    RB3.Text = bsc;
+                else if (RB4.Checked)
+                    RB3.Text = bba;
+                else if (RB5.Checked)
+                    RB3.Text = bcom;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {}
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (RB3.Checked)
+            {
+                
+                    comboBox1.Items.Add("COMPUTER SCIENCE");
+                    comboBox1.Items.Add("MATHEMATICS");
+                    comboBox1.Items.Add("PHYSICS");
+                    comboBox1.Items.Add("CHEMISTRY");
+                    comboBox1.Items.Add("BOTANY");
+                    comboBox1.Items.Add("ZOOLOGY");
+                    comboBox1.Items.Add("STATISTICS");
+                
+            }
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (RB4.Checked)
+            {
+                comboBox1.Items.Clear();
+                comboBox1.Items.Add("BUSSINESS STATISTICS");
+                comboBox1.Items.Add("BUSSINESS MATHEMATICS");
+                comboBox1.Items.Add("TALLY");
+                comboBox1.Items.Add("EXCEL");
+                comboBox1.Items.Add("ECONOMICS");
+            }
+
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            if (RB5.Checked)
+            {
+                comboBox1.Items.Clear();
+                comboBox1.Items.Add("Accountancy");
+                comboBox1.Items.Add("Statistics");
+                comboBox1.Items.Add("Management");
+                comboBox1.Items.Add("Human Resource");
+                comboBox1.Items.Add("ECONOMICS");
+            }
+        }
+
+       /* private void button1_Click_1(object sender, EventArgs e)
+        {
+            String name = textBox1.Text;
+            double regNo = double.Parse(textBox2.Text);
+            String state = comboBox2.Text;
+            String course = comboBox1.Text;
+            String room = comboBox3.Text;
+          }*/
+    }
+
+    internal class RESET_Click
+    {
+    }
+
+    internal class button1_Click_1
+    {
+    }
+}
---
+++ WindowsFormsApp2/Form2.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Linen;
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.Black;
+            this.groupBox1.Location = new System.Drawing.Point(199, 66);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(352, 547);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "FAMILY DETAILS";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(254, 511);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 26);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "NEXT >>";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Location = new System.Drawing.Point(163, 401);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(180, 87);
+            this.richTextBox1.TabIndex = 14;
+            this.richTextBox1.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 407);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 19);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Permanent Address";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 116);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(115, 19);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Mother\'s Name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 171);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(170, 19);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Father\'s contact number";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 227);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(177, 19);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Mother\'s contact number";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 286);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 19);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Parent\'s E-mail id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 345);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(148, 19);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Father\'s Designation";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 19);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Father\'s Name";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(191, 110);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(152, 26);
+            this.textBox6.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(191, 339);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(152, 26);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(191, 280);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(152, 26);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(191, 224);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(152, 26);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(191, 168);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(152, 26);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(191, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(10, 511);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 25);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "<<BACK";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp2.Properties.Resources.HB_E;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(784, 699);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
